a383c7d31cba58eafc753f5e818212b2ce7df261
LaylaSCastro <laylacristinasilvacastro@gmail.com>
2025-01-20 19:31:15 -0300

    feat: create login page

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Login-Screen' title`, () => {
+  it(`should have the 'aula-1h' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Login-Screen');
+    expect(app.title).toEqual('aula-1h');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Login-Screen');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, aula-1h');
   });
 });
